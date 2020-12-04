86c4f0e3ceadf3435519380260cd8824c93f1807
Robbie Luijben <diggingforfire@gmail.com>
2020-12-04 13:01:20 +0100

    Day 4 part 1

---
+++ csharp/AdventOfCode2020/04.01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _04._01
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var validPasswordCount =
+                (await File.ReadAllTextAsync("input.txt"))
+                .Split($"{Environment.NewLine}{Environment.NewLine}")
+                .Select(p =>
+                    p.Split(' ')
+                        .SelectMany(r =>
+                            r.Split(Environment.NewLine))
+                        .Select(q => new {Parts = q.Split(':')})
+                        .Select(s => new {Name = s.Parts[0], Value = s.Parts[1]})
+                )
+                .Count(passport =>
+                    new[] {"ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt"}
+                    .All(field => passport.Any(passportField => passportField.Name == field)));
+
+            Console.WriteLine(validPasswordCount);
+        }
+    }
+}
+
