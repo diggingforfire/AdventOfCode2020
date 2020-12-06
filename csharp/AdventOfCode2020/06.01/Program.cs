9d3e676fca275dced4c95ea88ad0ffe49d9781e5
Robbie Luijben <diggingforfire@gmail.com>
2020-12-06 18:42:24 +0100

    Day 6

---
+++ csharp/AdventOfCode2020/06.01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+var count =
+    (await File.ReadAllTextAsync("input.txt"))
+    .Split($"{Environment.NewLine}{Environment.NewLine}")
+    .Select(group => group.Split(Environment.NewLine))
+    .Select(group => group.SelectMany(letter => letter))
+    .Select(group => group.GroupBy(letter => letter).Count())
+    .Sum();
+
+Console.WriteLine(count);
---
+++ csharp/AdventOfCode2020/06.02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+var count =
+    (await File.ReadAllTextAsync("input.txt"))
+    .Split($"{Environment.NewLine}{Environment.NewLine}")
+    .Select(group => group.Split(Environment.NewLine))
+    .Select(group => new
+    {
+        Answers = group,
+        UniqueLetters = group
+            .SelectMany(letter => letter)
+            .GroupBy(letter => letter)
+            .Select(letter => letter.Key)
+
+    })
+    .Sum(group =>
+        group.UniqueLetters.Count(letter => group.Answers.All(answer => answer.Contains(letter))));
+
+Console.WriteLine(count);
