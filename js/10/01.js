4960fd1307a8aa0e8f8cc0bb3c85a6947e6049ed
Robbie Luijben <diggingforfire@gmail.com>
2020-12-10 15:47:36 +0100

    Rename some vars

--- js/10/01.js
+++ js/10/01.js
@@ -8,7 +8,7 @@ input.push(Math.max.apply(null, input) + 3);
 
 const map = input
     .sort((a, b) => a - b)
-    .map((n, i, a) => a[i + 1] - n || 0)
+    .map((n, index, all) => all[index + 1] - n || 0)
     .filter((x) => !!x)
     .reduce(
         (entryMap, e) => entryMap.set(e, [...(entryMap.get(e) || []), e]),
