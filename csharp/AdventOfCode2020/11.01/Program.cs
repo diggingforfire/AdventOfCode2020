646f75f3311f2dceb81546cb8d02bdaf58fd038d
Robbie Luijben <diggingforfire@gmail.com>
2020-12-12 00:41:06 +0100

    Day 11

---
+++ csharp/AdventOfCode2020/11.01/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+var input =
+    (await File.ReadAllLinesAsync("input.txt"))
+    .Select(line => line.ToCharArray().Prepend('.').Append('.').ToArray()).ToArray();
+
+input = input
+    .Prepend(new string('.', input[0].Length).ToCharArray())
+    .Append(new string('.', input[0].Length).ToCharArray()).ToArray();
+
+bool changed = true;
+while (changed)
+{
+    changed = false;
+    var previousInput = input.Select(row => row.Select(@char => @char).ToArray()).ToArray();
+    
+    for (int y = 1; y < input.Length - 1; y++)
+    {
+        var line = input[y];
+        
+        for (int x = 1; x < line.Length - 1; x++)
+        {
+            var @char = line[x];
+            var adjacents = GetAdjacents(previousInput, y, x);
+
+            input[y][x] = 
+                @char == 'L' && !adjacents.Any(a => a == '#') ? '#' :
+                @char == '#' && adjacents.Count(a => a == '#') >= 4 ? 'L' : 
+                input[y][x];
+
+            changed |= input[y][x] != previousInput[y][x];
+        }
+    }
+}
+
+var count = input.Sum(row => row.Count(a => a == '#'));
+Console.WriteLine(count);
+
+static char[] GetAdjacents(char[][] input, int x, int y)
+{
+    return new[]
+    {
+        input[x][y - 1], input[x + 1][y - 1], input[x + 1][y], input[x + 1][y + 1],
+        input[x][y + 1], input[x - 1][y + 1], input[x - 1][y], input[x - 1][y - 1]
+    };
+}
---
+++ csharp/AdventOfCode2020/11.02/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _11._02
+{
+    class Seat { public char Char { get; set; } public Point Point { get; set; } }
+
+    class Program
+    {
+        static readonly (int x, int y)[] _adjacents = new[] { (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1) };
+
+        static Seat GetNextSeat(int y, int x, (int x, int y) offset, Seat[][] seats)
+        {
+            var newY = y + offset.y;
+            var newX = x + offset.x;
+            if (newY < 0 || newY >= seats.Length || newX < 0 || newX >= seats[newY].Length) return null;
+            var next = seats[newY][newX];
+            if (next.Char != '.') return next;
+            return GetNextSeat(newY, newX, offset, seats);
+        }
+
+        static async Task Main()
+        {
+            var points =
+                (await File.ReadAllLinesAsync("input.txt"))
+                .Select((line, y) => line.Select((c, x) => new Seat {Char = c, Point = new Point(x, y)}).ToArray())
+                .ToArray();
+
+            bool changed = true;
+            while (changed)
+            {
+                var previousSeats = points.Select(p => p.Select(b => new Seat {Char = b.Char, Point = b.Point}).ToArray()).ToArray();
+
+                changed = false;
+                for (int y = 0; y < points.Length; y++)
+                {
+                    var row = points[y];
+
+                    for (int x = 0; x < row.Length; x++)
+                    {
+                        var seat = row[x];
+                        if (seat.Char == '.') continue;
+                        var adjacents = _adjacents.Select(a => GetNextSeat(y, x, a, previousSeats)).ToArray();
+
+                        seat.Char =
+                            seat.Char == 'L' && !adjacents.Any(visiblePoint => visiblePoint?.Char == '#') ? '#' :
+                            seat.Char == '#' && adjacents.Count(visiblePoint => visiblePoint?.Char == '#') >= 5 ? 'L' :
+                            seat.Char;
+
+                        changed |= seat.Char != previousSeats[y][x].Char;
+                    }
+                }
+            }
+
+            var res = points.Sum(p => p.Count(a => a.Char == '#'));
+            Console.WriteLine(res);
+        }
+    }
+}
