96688130b6e6f8e58c7f6d6d7c9e9571fd1666d0
Robbie Luijben <diggingforfire@gmail.com>
2020-12-15 16:34:23 +0100

    Day 14 part 2

--- js/14/02.js
+++ js/14/02.js
@@ -1,6 +1,6 @@
 const input = require("fs")
-.readFileSync(require("path").join(__dirname, "input.txt"), "utf8")
-.split("\r\n");
+    .readFileSync(require("path").join(__dirname, "input.txt"), "utf8")
+    .split("\r\n");
 
 const sum = run(input, 0);
 
@@ -11,31 +11,51 @@ function run(input, index, mask, mem) {
     const next = input[index];
 
     if (next) {
-        if (next.startsWith('mask')) {
+        if (next.startsWith("mask")) {
             mask = next.split("=")[1].trimStart();
         } else {
             const address = next.match(/\[(.*?)\]/)[1];
             const value = next.split("=")[1].trimStart();
-            applyMask(address, mask).forEach(maskedAddress => {
-                mem[maskedAddress] = value;
+            applyMask(address, mask).forEach((maskedAddress) => {
+                mem[maskedAddress] = parseInt(value);
             });
         }
         return run(input, ++index, mask, mem);
     } else {
-        return Object.keys(mem).map(key => mem[key]).filter(x => !!x).reduce((a, b) => a + b);
+        return Object.keys(mem)
+            .map((key) => mem[key])
+            .filter((x) => !!x)
+            .reduce((a, b) => a + b);
     }
 }
 
 function applyMask(value, mask) {
-    const masked = 
-        (value >>> 0)
+    const maskedAddress = (value >>> 0)
         .toString(2)
-        .padStart(mask.length, '0')
-        .split('')
-        .map((bit, index) => 
-            mask[index] === '0' ? bit : 
-            mask[index] === '1' ? '1' :
-            'X');
-        
-    return parseInt(masked.join(""), 2);
+        .padStart(mask.length, "0")
+        .split("")
+        .map((bit, index) =>
+            mask[index] === "0" ? bit : mask[index] === "1" ? "1" : "X"
+        );
+
+    const xCount = maskedAddress.filter((c) => c === "X").length;
+    const cartesianProd = cartesian(
+        ...Array.from(new Array(xCount), () => [0, 1])
+    );
+
+    const maskedAddressString = maskedAddress.join("");
+
+    const res = cartesianProd.map((combination) => {
+        let newAdd = maskedAddressString;
+        combination.forEach((val) => {
+            newAdd = newAdd.replace("X", val);
+        });
+        return parseInt(newAdd, 2);
+    });
+
+    return res;
+}
+
+function cartesian(...a) {
+    return a.reduce((a, b) => a.flatMap((d) => b.map((e) => [d, e].flat())));
 }
