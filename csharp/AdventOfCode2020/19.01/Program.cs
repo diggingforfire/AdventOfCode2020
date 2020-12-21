301349a71bafc479c189bb357c2926e3c91a91a3
Robbie Luijben <diggingforfire@gmail.com>
2020-12-21 15:32:46 +0100

    19.1 and 20.1

---
+++ csharp/AdventOfCode2020/19.01/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _19._01
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var parts =
+                (await File.ReadAllTextAsync("input.txt"))
+                .Split($"{Environment.NewLine}{Environment.NewLine}")
+                .Select(line => line.Split(Environment.NewLine))
+                .ToArray();
+
+            var rules = parts[0].Select(line =>
+            (
+                Id: int.Parse(line.Substring(0, line.IndexOf(':'))),
+                Letter: line.Contains('"') ? line[^2].ToString() : null,
+                SubRules: !line.Contains('"')
+                    ? line.Substring(line.IndexOf(':') + 1).Split("|").Select(rulePart =>
+                            rulePart.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray())
+                        .ToArray()
+                    : new int[][] { }
+            )).ToDictionary(tuple => tuple.Id);
+
+            var messages = parts[1];
+
+            var validMessages = GetValidMessages(rules[0], rules);
+
+            var validCount = messages.Count(message => validMessages.Contains(message));
+
+            Console.WriteLine(validCount);
+        }
+
+        static string[] GetValidMessages(
+            (int Id, string Letter, int[][] SubRules) rule, 
+            IDictionary<int, (int Id, string Letter, int[][] SubRules)> rules, int depth = 0)
+        {
+            depth++;
+
+            if (rule.Letter != null)
+            {
+                return new[] {rule.Letter};
+            }
+
+            List<string> list = new List<string>();
+
+            foreach (var subRuleGroup in rule.SubRules)
+            {
+                var message = subRuleGroup.Select(subRuleId => GetValidMessages(rules[subRuleId], rules, depth)).ToArray();
+                var cartesianProduct = CartesianProduct(message).Select(c => string.Join("", c)).ToArray();
+                list.AddRange(cartesianProduct);
+            }
+            
+            return list.ToArray();
+        }
+
+        static IEnumerable<IEnumerable<T>> CartesianProduct<T>
+            (IEnumerable<IEnumerable<T>> sequences)
+        {
+            IEnumerable<IEnumerable<T>> emptyProduct =
+                new[] { Enumerable.Empty<T>() };
+            return sequences.Aggregate(
+                emptyProduct,
+                (accumulator, sequence) =>
+                    from accseq in accumulator
+                    from item in sequence
+                    select accseq.Concat(new[] { item }));
+        }
+    }
+}
---
+++ csharp/AdventOfCode2020/20.01/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _20._01
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var tiles =
+                (await File.ReadAllTextAsync("input.txt"))
+                .Split($"{Environment.NewLine}{Environment.NewLine}")
+                .Select(line => line.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+                .Select(tileLines => new Tile(
+                    int.Parse(Regex.Match(tileLines[0], "([0-9]{4})").Value),
+                    tileLines.Skip(1).Select(line => line.Select(c => c).ToArray()).ToArray()))
+                .ToArray();
+
+            var cornerTiles = tiles.Where(tile1 =>
+            {
+                var alignedTileCount = tiles.Where(tile2 => tile1 != tile2).Count(tile2 =>
+                    tile1.Sides.Any(side1 => tile2.Sides.Any(side2 => side1 == side2))
+                );
+
+                return alignedTileCount == 2;
+            });
+
+            var multipliedIds = cornerTiles.Select(tile => tile.Id).Aggregate((a, b) => a * b);
+
+            Console.WriteLine(multipliedIds);
+        }
+        
+        class Tile
+        {
+            public Tile(int id, char[][] data)
+            {
+                Id = id;
+
+                var top = ToBinary(data[0]);
+                var bottom = ToBinary(data[^1]);
+                var right = ToBinary(data.Select(chars => chars[^1]).ToArray());
+                var left = ToBinary(data.Select(chars => chars[0]).ToArray());
+
+                var dataFlippedHorizontally = data.Select(chars => chars.Reverse().ToArray()).ToArray();
+                var dataFlippedVertically = dataFlippedHorizontally.Reverse().ToArray();
+
+                var topFlipped = ToBinary(dataFlippedVertically[0]);
+                var bottomFlipped = ToBinary(dataFlippedVertically[^1]);
+                var rightFlipped = ToBinary(dataFlippedVertically.Select(chars => chars[^1]).ToArray());
+                var leftFlipped = ToBinary(dataFlippedVertically.Select(chars => chars[0]).ToArray());
+
+                Sides = new[] {top, bottom, right, left, topFlipped, bottomFlipped, rightFlipped, leftFlipped};
+            }
+
+            public int[] Sides { get; }
+
+            public long Id { get; }
+
+            private static int ToBinary(char[] chars)
+            {
+                var bin = string.Join("", chars.Select(c => c == '#' ? '1' : '0'));
+                return Convert.ToInt32(bin, 2);
+            }
+        }
+    }
+}
