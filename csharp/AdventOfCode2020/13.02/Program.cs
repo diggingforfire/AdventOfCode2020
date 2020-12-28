1a4e3ed944961cf9c8da85498f8829ccbe105ad4
Robbie Luijben <diggingforfire@gmail.com>
2020-12-28 03:27:29 +0100

    Day 13 - 2

--- csharp/AdventOfCode2020/13.02/Program.cs
+++ csharp/AdventOfCode2020/13.02/Program.cs
@@ -1,25 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace _13._02
 {
-    class Holder
-    {
-        public NumberWithIndex One { get; set; }
-        public NumberWithIndex Two { get; set; }
-        public long Multiple { get; set; }
-    }
-
-    class NumberWithIndex
-    {
-        public int Number { get; set; }
-        public int Index { get; set; }
-    }
-
+    
     class Program
     {
         static async Task Main()
@@ -29,142 +16,25 @@ namespace _13._02
                 .Select(line =>
                     line.Split(",")
                         .Select((str, index) => new {str, index}).Where(a => a.str != "x")
-                        .Select(a => new{ Number = int.Parse(a.str), Index = a.index }
+                        .Select(a => new{ Number = long.Parse(a.str), Index = a.index }
                        ).ToArray())
                 .ToArray()[1];
 
+            long stepSize = busIds[0].Number;
+            long timestamp = busIds[0].Number;
 
-            var holders = new List<Holder>();
-
-            foreach (var id in busIds)
+            // https://en.wikipedia.org/wiki/Chinese_remainder_theorem#Search_by_sieving
+            foreach (var busId in busIds.Skip(1))
             {
-                foreach (var otherId in busIds)
+                while ((timestamp + busId.Index) % busId.Number != 0)
                 {
-                    if (id.Number == otherId.Number) continue;
-                    if ( (id.Index - otherId.Index) % id.Number == 0)
-                    {
-                        holders.Add(new Holder
-                        {
-                            Multiple = id.Number * otherId.Number,
-                            One = new NumberWithIndex {Number = id.Number, Index = id.Index},
-                            Two = new NumberWithIndex {Number = otherId.Number, Index = otherId.Index}
-                        });
-                      
-
-                    }
+                    timestamp += stepSize;
                 }
+
+                stepSize *= busId.Number;
             }
 
-            Parallel.ForEach(holders, (holder) =>
-            {
-                for (long i = 1; i < long.MaxValue; i++)
-                {
-                    if (i % holder.Multiple == 0)
-                    {
-                        var count = busIds.Count(id =>
-                        {
-                            int altIndex = holder.Two.Index - id.Index;
-                            bool testval = (i - altIndex) % id.Number == 0;
-                            return testval;
-                        });
-
-                        if (count == busIds.Length)
-                        {
-
-                        }
-                        //if (test)
-                        //{
-                        //    Console.WriteLine($"yas");
-                        //}
-                    }
-                }
-            });
-
-
-
-
-            var sw = Stopwatch.StartNew();
-
-            Parallel.For(1, long.MaxValue, (i) =>
-            {
-                foreach (var holder in holders)
-                {
-                    if (i % holder.Multiple == 0)
-                    {
-                        bool test = busIds.All(id =>
-                        {
-                            int altIndex = holder.One.Index - id.Index;
-                            bool testval = (i - altIndex) % id.Number == 0;
-                            return testval;
-                        });
-
-
-
-                        if (test)
-                        {
-                            Console.WriteLine($"yas");
-                        }
-                    }
-
-                    if (i % 10000000 == 0)
-                    {
-                        Console.WriteLine($"{(i / (double)long.MaxValue) * 100}");
-                    }
-                }
-            });
-            //for (long i = 1; i < long.MaxValue; i++)
-            //{
-            //    foreach (var holder in holders)
-            //    {
-            //        if (i % holder.Multiple == 0)
-            //        {
-            //            bool test = busIds.All(id =>
-            //            {
-            //                int altIndex = holder.One.Index - id.Index;
-            //                bool testval = (i - altIndex) % id.Number == 0;
-            //                return testval;
-            //            });
-
-                       
-
-            //            if (test)
-            //            {
-            //                Console.WriteLine($"yas");
-            //            }
-            //        }
-
-            //        if (i % 10000000 == 0)
-            //        {
-            //            Console.WriteLine($"{(i / long.MaxValue) * 100}");
-            //        }
-            //    }
-                //var test = holders.Any(holder =>
-                //{
-
-                //    return true;
-                //});
-                //if (multiples.All(mul => mul % multiple == 0))
-                //{
-
-                //}
-                //bool test = busIds.All(id =>
-                //{
-                //    int altIndex = index - id.index;
-                //    bool testval = (i - altIndex) % id.number == 0;
-                //    return testval;
-                //});
-
-                //if (i % 10000000 == 0)
-                //{
-                //    Console.WriteLine($"{(i/ long.MaxValue)*100}");
-                //}
-                //if (test)
-                //{
-                //    Console.WriteLine(i - index);
-                //    sw.Stop();
-                //    Console.WriteLine(sw.ElapsedMilliseconds);
-                //    break;
-                //}
-            }
+            Console.WriteLine(timestamp);
+        }
     }
 }
